63b9744e50280888f3fb39bfbbc21fd8d23593f2
Alejandro <alejandro.granados.c@gmail.com>
2018-05-09 12:48:46 -0700

    test

--- bitVStrit.R
+++ bitVStrit.R
@@ -9,16 +9,17 @@ source("simMemoirStrDist2.R")
 source("simulation2.R")
 source("MLfunctions.R")
 library(doParallel)
+
 registerDoParallel(cores=8)
 
 #SET PARAMETERS
 barcodes = c(2,4,6,8,10,15,20,25,50,100)
-barcodes= c(6,10)
+barcodes= c(6,10,50)
 generations=c(3,4,5,6,7,8)
-generations=c(8)
+#generations=c(8)
 mus = c(0.9999,0.9,0.8,0.7,0.6,0.5,0.4,0.3,0.2,0.1,0.0001)
 #mus = c(0.7)#,0.6)#,0.5,0.4,0.2,0.1)
-mus =c(0.999999)
+#mus =c(0.999999)
 #barcodes = c(6,7)
 #generations = c(7,8)
 nRepeats=20
