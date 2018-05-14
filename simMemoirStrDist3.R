e88bdecb99c5f7694d26384ae6ae180cb2dde5a9
Alejandro <alejandro.granados.c@gmail.com>
2018-05-13 23:50:51 -0700

    is FUc**ng working now in parallel. THe heatmap.2 function does not work in parallalel

--- simMemoirStrDist3.R
+++ simMemoirStrDist3.R
@@ -20,6 +20,7 @@ library(phangorn)
 library(stringdist)
 library(doParallel)
 library(gplots)
+source("MLfunctions.R")
 source("simulation2.R")
 
 compareDist <- function(simulationType='trit',nGen=3,mu=0.4,alpha_=2/3,barcodeLength=6,nRepeats=20,methods=c('osa','lv','dl','hamming','lcs','qgram','cosine','jaccard','jw','soundex')){
@@ -217,11 +218,16 @@ simMemoirStrdist<-function(nGen,mu,alpha,barcodeLength,methods,simulationType){
   
   #try new distance using the built in dendrogram of heatmap2
   #h=heatmap.2(matdist_,trace="none",dendrogram = 'column')
-  h=heatmap.2(matdist_+t(matdist_))
-  heatmap.tree=as.phylo(as.hclust(h$colDendrogram))
-  heatmap.tree$tip.label = treeUPGMA$tip.label
-  allDistances[m+3]= RF.dist(removeSeqLabel(heatmap.tree),trueTree)
-  
+  # h=heatmap.2(matdist_+t(matdist_),Colv="Rowv")
+  # heatmap.tree=as.phylo(as.hclust(h$colDendrogram))
+  # heatmap.tree$tip.label = treeUPGMA$tip.label
+  # allDistances[m+3]= RF.dist(removeSeqLabel(heatmap.tree),trueTree)
+  # 
+  #alternative w/o plotting the actual heatmap, only hclust method
+  hclust.tree=as.phylo(hclust(as.dist(t(matdist_))))
+  hclust.tree$tip.label = treeUPGMA$tip.label
+  allDistances[m+3]= RF.dist(removeSeqLabel(hclust.tree),trueTree)
+
     print("All distances calcualted")
   
     
