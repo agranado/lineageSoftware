ab9dc29107e158df8abaf4e487a66d1712c59688
Alejandro <alejandro.granados.c@gmail.com>
2018-10-11 15:58:15 -0700

    changes made for the plots used at Allen's retreat

--- MLfunctions.R
+++ MLfunctions.R
@@ -104,6 +104,136 @@ manualDistML <- function(barcodeLeaves,mu,alpha,nGen){
       Pr.s1_s2=array()
       Pr.ind=array()
       for (s in 1:barcodeLength){
+        #NEW section for W
+        if(barcodeArray1[s]=="w" | barcodeArray2[s]=="w"){ #any of them is W
+
+
+          #if the first one is w
+          if(barcodeArray1[s]=="w" & barcodeArray2[s]=="w"){
+            sumPr=1;Pr.ind[s] =1
+          }else if(barcodeArray1[s]=="w"){
+              Pr.ind[s]=Pr[which(alphabet ==barcodeArray2[s])] # Pr(non-w char) * Pr(w)\\
+
+              s2=which(alphabet==barcodeArray2[s])
+
+              #sum over all possible S0 states
+              sumPr=0
+              for(a in 1:length(alphabet)){
+                 sumPr = sumPr+ Tran.pr[a,s2] * Pr_s0_array[a]   # Pr_s0(alphabet[a],mu,alpha,nGen-1)
+              }
+
+
+          }else if(barcodeArray2[s]=="w"){
+              Pr.ind[s]=Pr[which(alphabet ==barcodeArray1[s])]
+
+
+              s1=which(alphabet==barcodeArray1[s])
+
+              sumPr=0
+              for(a in 1:length(alphabet)){
+                 sumPr = sumPr+ Tran.pr[a,s1] * Pr_s0_array[a]   # Pr_s0(alphabet[a],mu,alpha,nGen-1)
+              }
+
+          }
+
+        }else{
+
+          #NORMAL
+
+            Pr.ind[s] = PrMatrix[which(alphabet ==barcodeArray1[s]),which(alphabet ==barcodeArray2[s])]
+            #Pr of observing these characters independtly arising Pr1 * Pr2 : assuming independence at nGen
+            # Sum{i=1}{3} Pr(s1|S0=i)Pr(s2|s0=i)Ps(S0=i)
+            s1=which(alphabet==barcodeArray1[s])
+            s2=which(alphabet==barcodeArray2[s])
+
+            #sum over all possible S0 states
+            sumPr=0
+            for(a in 1:length(alphabet)){
+               sumPr = sumPr+ Tran.pr[a,s1] * Tran.pr[a,s2] * Pr_s0_array[a]   # Pr_s0(alphabet[a],mu,alpha,nGen-1)
+            }
+        }
+        Pr.s1_s2[s] =sumPr
+
+      } #end barcode loop
+        ratio.product=sum(log(Pr.s1_s2))/sum(log(Pr.ind))
+
+      distMat[i,j]= distSum
+      ratioMat[i,j]=1/ratio.sum *distSum
+      productMat[i,j] = ratio.product
+
+    }
+  }
+  return(productMat)
+}
+
+
+manualDistML__ <- function(barcodeLeaves,mu,alpha,nGen){
+  alphabet = array()
+  alphabet[1]= "u"
+  alphabet[2]= "r"
+  alphabet[3]= "x"
+  #mu & alpha indicate an explicit probabilistic model
+  #Probability of no mutation for nGen cell divisions (mu is rate of edit per cell division)
+
+  #transition probabilities
+
+  #this is of the form i,j   Pr(i->j | i)
+  Tran.pr = t(matrix(c(1-mu, mu*alpha,mu*(1-alpha),0,1,0,0,0,1),nrow=3,ncol=3))
+
+
+  #NULL MODEL: probability of observing sites as independent events:
+  Pr = array()
+  Pr[1] = (1-mu)^nGen
+  #probability of nu mutation during nGen-1 devisions and then a mutation in generation nGen times Pr(alphabet[2])
+  #then we use the choose to correct for all the order in which this could have happened, (we need still further correction to
+  #to account for irreversibility)
+  #Pr[2] = choose(nGen,nGen-1)*( 1-mu)^(nGen-1)*mu*alpha
+  #corrected for irreversibility:
+  Pr[2] = Pr_edit(nGen,mu,alpha)
+
+  #same as before but using (1-alpha)
+  #Pr[3] = choose(nGen,nGen-1)*(1-mu)^(nGen-1)*mu*(1-alpha)
+  Pr[3] = Pr_edit(nGen,mu,1-alpha)
+
+  PrMatrix  = array(0,dim=c(length(Pr),length(Pr)))
+  #calcualte probabilistic model:
+  #this just means the pr that those two sites have those characters by random. This is the expected pr for each site
+  #it assummes independence but does not tell you how likely they are to come from a common ancestor
+  for (p1 in 1:length(alphabet)){
+    for (p2 in 1:length(alphabet)){
+      PrMatrix[p1,p2] = Pr[p1] * Pr[p2]
+    }
+  }
+
+  #weights for number of sustitutions
+  equalU =0
+  oneSust = 1
+  twoSust = 2
+
+  nBarcodes = length(barcodeLeaves)
+  barcodeLength=nchar(barcodeLeaves[1])
+  distMat= array(0,dim =c(nBarcodes,nBarcodes))
+  ratioMat = array(0,dim=c(nBarcodes,nBarcodes))
+  productMat = array(0,dim=c(nBarcodes,nBarcodes))
+
+  Pr_s0_array = array()
+  for (i in 1:length(alphabet)){
+    Pr_s0_array[i]= Pr_s0(alphabet[i], mu, alpha, nGen-1)
+  }
+  #go through all the elements in the barcode array
+  for (i in 1:(nBarcodes-1)){
+    barcodeArray1 =strsplit(barcodeLeaves[i],"")[[1]]
+    for (j in (i+1):nBarcodes){
+
+      barcodeArray2 =strsplit(barcodeLeaves[j],"")[[1]]
+      distSum = 1
+      ratio.sum =0
+      ratio.product=1
+      #for each pairwise comparison
+      Pr.s1_s2=array()
+      Pr.ind=array()
+      for (s in 1:barcodeLength){
+
         Pr.ind[s] = PrMatrix[which(alphabet ==barcodeArray1[s]),which(alphabet ==barcodeArray2[s])]
         #Pr of observing these characters independtly arising Pr1 * Pr2 : assuming independence at nGen
         # Sum{i=1}{3} Pr(s1|S0=i)Pr(s2|s0=i)Ps(S0=i)
@@ -116,7 +246,7 @@ manualDistML <- function(barcodeLeaves,mu,alpha,nGen){
            sumPr = sumPr+ Tran.pr[a,s1] * Tran.pr[a,s2] * Pr_s0_array[a]   # Pr_s0(alphabet[a],mu,alpha,nGen-1)
         }
         Pr.s1_s2[s] =sumPr
-      }
+      } #end barcode loop
         ratio.product=sum(log(Pr.s1_s2))/sum(log(Pr.ind))
 
       distMat[i,j]= distSum
--- mainPlots.R
+++ mainPlots.R
@@ -21,7 +21,7 @@ source("simMemoirRandomGuess.R")
  # a = array()
  # for(ng in 1:length(generations)){
  #   b[ng,]=simMemoirRandomGuess(generations[ng],mu,alpha,barcodes[1],nRepeats)
- #   a[ng] = median(b[ng,]) 
+ #   a[ng] = median(b[ng,])
  # }
 a=c(2    ,9,   26,   58,  122,  250,  506, 1018, 2042)
 
@@ -41,17 +41,17 @@ mIdx=7
 simType=muVariation[[mIdx]]
 par(mfrow=c(2,5))
 for(ng in 1:length(generations)){
-  
+
   for(bc in 1:length(barcodes)){
     #distBit[bc,ng]=apply(simType[['binary']][[bc]][[ng]],2,mean)[11]
     distMat[bc,ng,]=apply(simType[['trit']][[bc]][[ng]],2,mean) # 12 -> RF.dist using manualDist + UPGMA
-    
+
   }
-  
+
   matplot(log(barcodes),distMat[,ng,],main=paste("gen = ",toString(generations[ng]),sep=""),
        type="o",ylim=c(0,a[ng]+0.05*a[ng]),ylab="RF dist",xlab="log N scratchpads",
        cex.lab=1.5,cex.axis=1.5,cex.main=2,lwd=1.5)
- 
+
   abline(h=a[ng],col="red",lwd=1.5)
 }
 
@@ -63,21 +63,21 @@ distMat=array(0,dim=c(length(barcodes),length(generations),3))
 simType=muVariation[[mIdx]]
 par(mfrow=c(2,5))
 for(ng in 1:length(generations)){
-  
+
   for(bc in 1:length(barcodes)){
     #distBit[bc,ng]=apply(simType[['binary']][[bc]][[ng]],2,mean)[11]
     distMat[bc,ng,]=apply(simType[['trit']][[bc]][[ng]],2,mean) # 12 -> RF.dist using manualDist + UPGMA
-    
+
   }
-  
+
   matplot(log10(barcodes),distMat[,ng,],main=paste("gen = ",toString(generations[ng]),sep=""),
           type="o",ylim=c(0,a[ng]+0.05*a[ng]),ylab="RF dist",xlab="log N scratchpads",
           cex.lab=1.5,cex.axis=1.5,cex.main=2,lwd=1.5,col = darkcols)
-  
-  
+
+
   legend("right",as.character(c(1,2,3)),col=darkcols,cex=0.8,fill=darkcols,title="edit rate")
   abline(h=a[ng],col="red",lwd=1.5)
-  
+
 }
 #
 #
@@ -89,21 +89,21 @@ distMat=array(0,dim=c(length(barcodes),length(generations),3))
 simType=muVariation[[mIdx]]
 par(mfrow=c(2,5))
 for(ng in 1:length(generations)){
-  
+
   for(bc in 1:length(barcodes)){
     #distBit[bc,ng]=apply(simType[['binary']][[bc]][[ng]],2,mean)[11]
     distMat[bc,ng,]=apply(simType[['trit']][[bc]][[ng]]==0,2,sum)/dim(simType[['trit']][[bcIdx]][[1]]==0)[gIdx] # 12 -> RF.dist using manualDist + UPGMA
-    
+
   }
-  
+
   matplot(log10(barcodes),distMat[,ng,],main=paste("gen = ",toString(generations[ng]),sep=""),
           type="o",ylim=c(0,1),ylab="Prop perfect trees",xlab="log N scratchpads",
           cex.lab=1.5,cex.axis=1.5,cex.main=2,lwd=1.5,col = darkcols)
-  
-  
+
+
   legend("right",as.character(c(1,2,3)),col=darkcols,cex=0.8,fill=darkcols,title="edit rate")
   abline(h=a[ng],col="red",lwd=1.5)
-  
+
 }
 
 
@@ -125,19 +125,19 @@ pathPlots="/Users/alejandrog/MEGA/Caltech/trees/simulation/plots/meetingMay15/"
 simType=muVariation[[mIdx]]
 par(mfrow=c(2,5))
 for(ng in 1:length(generations)){
-  
+
   for(bc in 1:length(barcodes)){
     #distBit[bc,ng]=apply(simType[['binary']][[bc]][[ng]],2,mean)[11]
     distTrit[bc,ng]=apply(simType[['trit']][[bc]][[ng]],2,mean)[manual.idx] # 12 -> RF.dist using manualDist + UPGMA
     distBit[bc,ng]=apply(simType[['binary']][[bc]][[ng]],2,mean)[upgam.idx] #11 -> RF.dist using dist.ml + UPGMA
   }
-  
+
   plot(log(barcodes),distBit[,ng],main=paste("gen = ",toString(generations[ng]),sep=""),
        type="o",ylim=c(0,a[ng]+0.05*a[ng]),ylab="RF dist",xlab="log N scratchpads",
        cex.lab=1.5,cex.axis=1.5,cex.main=2,lwd=1.5)
   lines(log(barcodes),distTrit[,ng],type="o",col="blue",lwd=1.5)
-  
-  
+
+
   abline(h=a[ng],col="red",lwd=1.5)
 }
 dev.off()
@@ -181,18 +181,18 @@ subplot(p1,p2)
 
 dev.off()
 
-#compare the different distance measures: 
+#compare the different distance measures:
 distAllCompare = array(0,dim=c(3,length(generations),length(mus)))
 par(mfrow=c(2,3))
 for(ng in 1:length(generations)){
   for (mIdx in 1:length(mus)){
     simType=muVariation[[mIdx]]
-    
+
     for(d in 1:3){
       #distBitAll[bc,ng,mIdx]=apply(simType[['binary']][[bc]][[ng]],2,mean)[12] #12 is normal UPGMA + as.dist
       distAllCompare[d,ng,mIdx]=apply(simType[['trit']][[bc]][[ng]],2,mean)[d]
       #distBitAll[bc,ng, mIdx]=apply(simType[['trit']][[bc]][[ng]],2,mean)[12]
-      
+
     }
   }
   #plot(mus,1-distTritAll[3,ng,]/a[ng],ylim=c(0,1),
@@ -200,10 +200,10 @@ for(ng in 1:length(generations)){
   #     main=paste("g=",toString(generations[ng]),sep=""),
   #     cex.axis=1.5,cex.lab=1.6,cex.main=2);
   #lines(mus,1-distBitAll[3,ng,]/a[ng],type="o")
-  
+
   #lines(mus,1-distTritAll[5,ng,]/a[ng],col="#3399FF",pch=17,lty=6)
   #lines(mus,1-distBitAll[5,ng,]/a[ng],col="gray",pch=17,lty=6)
-  
+
 }
 
 
@@ -219,12 +219,12 @@ par(mfrow=c(2,5))
 for(ng in 1:length(generations)){
   for (mIdx in 1:length(mus)){
     simType=muVariation[[mIdx]]
-    
+
     for(bc in 1:length(barcodes)){
       distBitAll[bc,ng,mIdx]=apply(simType[['binary']][[bc]][[ng]],2,mean)[1] #12 is normal UPGMA + as.dist
       distTritAll[bc,ng,mIdx]=apply(simType[['trit']][[bc]][[ng]],2,mean)[3]
-      
-      
+
+
     }
   }
   plot(mus,1-distTritAll[BCn,ng,]/a[ng],ylim=c(0,1),
@@ -232,10 +232,10 @@ for(ng in 1:length(generations)){
        main=paste("g=",toString(generations[ng]),sep=""),
        cex.axis=1.5,cex.lab=1.8,cex.main=2,lwd=2,cex.axis=1.5);
   lines(mus,1-distBitAll[BCn,ng,]/a[ng],type="o",lwd=2)
-  
+
   #lines(mus,1-distTritAll[5,ng,]/a[ng],col="#3399FF",pch=17,lty=6)
   #lines(mus,1-distBitAll[5,ng,]/a[ng],col="gray",pch=17,lty=6)
-  
+
 }
 dev.off()
 #take minimun across all edit rates
@@ -292,7 +292,7 @@ dev.off()
 pdf(paste(pathPlots,"RFdist_vsBC_optimalRate_vsEmpiricalTRIT_MEAN.pdf",sep=""))
 par(mfrow =c(2,5))
 barcodeAxis= log10(barcodes)
-for(ng in 1:length(generations)){ #generations to plot length(generations) : to plot all 
+for(ng in 1:length(generations)){ #generations to plot length(generations) : to plot all
   plot(barcodeAxis,(a[ng]-optimMatTrit[,ng])/a[ng],
        cex.main=2,cex.lab=1.8,cex.axis=1.5,type="o",
        col="blue",ylim=c(0,1),cex=1.5,main=paste("gen=",toString(generations[ng]),sep=""),
@@ -306,7 +306,7 @@ dev.off()
 pdf(paste(pathPlots,"RFdist_vsBC_optimalRate_vsEmpiricalTRIT.pdf",sep=""))
 par(mfrow =c(2,2))
 barcodeAxis= log(barcodes)
-for(ng in 1:length(1:2)){ #generations to plot length(generations) : to plot all 
+for(ng in 1:length(1:2)){ #generations to plot length(generations) : to plot all
   plot(barcodeAxis,(a[ng]-optimMatTrit[,ng])/a[ng],
        cex.main=2,cex.lab=1.5,cex.axis=1.5,type="o",
        col="blue",ylim=c(0,1),cex=1.5,main=paste("gen=",toString(generations[ng]),sep=""),
@@ -329,10 +329,10 @@ for(o in 1:dim(optimMatBit)[2]){
   optimMatTritNorm[,o]=optimMatTrit[,o]/a[o]
 }
 
-#subplots 
-#once the optimal reates were taken into account *by considering the minimum of the distance 
+#subplots
+#once the optimal reates were taken into account *by considering the minimum of the distance
 #across all values that were simulated, we can plot the distance in the color axis and consider
-#the number of scratchpads and the number of gnerations in the x and y axis. 
+#the number of scratchpads and the number of gnerations in the x and y axis.
 #this plot is from c(0,1), 1->perfect reconstruction; 0->random guess
 
 pdf(paste(pathPlots,"heatmaps_blue_BitvsTrit_atOptimalRates.pdf"))
@@ -357,7 +357,7 @@ for (ng in 1:length(generations)){
       muVar[bc,muIdx]= apply(muVariation[[muIdx]][['trit']][[bc]][[ng]],2,mean)[12]
     }
   }
-  plotList[[ng]]=plot_ly(y=as.character(log(barcodes)),x=as.character(mus),z=muVar,type="heatmap") 
+  plotList[[ng]]=plot_ly(y=as.character(log(barcodes)),x=as.character(mus),z=muVar,type="heatmap")
   matplot(log(barcodes),muVar)
 }
 legend("right",as.character(mus),col=seq_len(length(mus)),cex=0.8,fill=seq_len(length(mus)),title="edit rate")
@@ -365,28 +365,31 @@ legend("right",as.character(mus),col=seq_len(length(mus)),cex=0.8,fill=seq_len(l
 #matplot(mus,t(apply(muVarMatrix,c(1,2),mean)),type="o",pch=1:4)
 #legend("right",distNames,col=seq_len(length(distNames)),cex=0.8,fill=seq_len(length(distNames)),title="Generations")
 
-
-
+#paper plot
+# HHMI plots / retreat plots
 #how many generations you can do with N barcodes
 par(mfrow=c(2,2))
 optimMatNorm= array(0, dim=dim(optimMatTrit) ); for(i in 1:dim(optimMatTrit)[2]){ optimMatNorm[,i] = (a[i]-optimMatTrit[,i])/a[i] }
 optimMatBitNorm= array(0, dim=dim(optimMatTrit) ); for(i in 1:dim(optimMatBit)[2]){ optimMatBitNorm[,i] = (a[i]-optimMatBit[,i])/a[i] }
-t=c(0.80,0.85,0.90,0.95)  
-ylims=c(100,100,100,200)
+t=c(0.80,0.85,0.90,0.95)
+#ylims=c(100,100,100,200
+ylims=c(60,60,60,100)
 legend.y.pos = c(90,90,90,180)
 for(tt in 1:length(t)){
 
-   
+
     threshold=t[tt]
-    bc.per.gen.bit = array();for(i in 1:dim(optimMatBitNorm)[2]){ bc.per.gen.bit[i] = min( which(optimMatBitNorm[,i]>threshold) ) }
+    #bc.per.gen.bit = array();for(i in 1:dim(optimMatBitNorm)[2]){ bc.per.gen.bit[i] = min( which(optimMatBitNorm[,i]>threshold) ) }
+    bc.per.gen.bit = array();for(i in 1:dim(optimMatNorm)[2]){ bc.per.gen.bit[i] = min( which(optimMatNorm[,i]>t[3]) ) }
+    bc.per.gen.bit2 = array();for(i in 1:dim(optimMatNorm)[2]){ bc.per.gen.bit2[i] = min( which(optimMatNorm[,i]>t[1]) ) }
     bc.per.gen.trit = array();for(i in 1:dim(optimMatNorm)[2]){ bc.per.gen.trit[i] = min( which(optimMatNorm[,i]>threshold) ) }
-    
-    plot(generations,barcodes[bc.per.gen.bit],type="l",lwd=2.5,
+
+    plot(generations,barcodes[bc.per.gen.trit],type="l",lwd=2.5,
          xlim=c(2,10),ylim=c(2,ylims[tt]),cex.lab=1.5,cex.axis=1.2,ylab="Array length",xlab="Generations",
          main=paste(toString(t[tt])," % reconstructability",sep=""))
     grid (10,10, lty = 6, col = "cornsilk2")
     lines(generations,barcodes[bc.per.gen.bit],type="l",col="black",lwd=2)
     lines(generations,barcodes[bc.per.gen.trit],type="l",col="blue",lwd=2)
-   
-    legend(3,legend.y.pos[tt],c("Trit","Bit"),fill=c("blue","black"))
+    lines(generations,barcodes[bc.per.gen.bit2],type="l",col="gray",lwd=2)
+    #legend(3,legend.y.pos[tt],c("Trit","Bit"),fill=c("blue","black"))
 }
